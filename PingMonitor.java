d5d61ee6e1c080015691a1d876d16bd0dee9ac49
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2017-04-29 09:05:04 +0545

    Adds GUI and offline random monitor

--- PingMonitor.java
+++ PingMonitor.java
@@ -1,9 +1,12 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
 
 public class PingMonitor {
 
-  public static void runSystemCommand(String command) {
+  public static int runSystemCommand(String command) {
 
 		try {
 			Process p = Runtime.getRuntime().exec(command);
@@ -13,12 +16,14 @@ public class PingMonitor {
 			String s = "";
 			// reading output stream of the command
 			while ((s = inputStream.readLine()) != null) {
+				if(s.indexOf("could not find host")!=-1){
+					return -1;
+				}
 				int start = s.indexOf("time=");
 				if(start!=-1){
 					int end = s.indexOf("ms");
 					s = s.substring(start+5,end);
-					int ping = Integer.parseInt(s);
-					System.out.println(ping+"ms");
+					return Integer.parseInt(s);
 				}
 			}
 
@@ -27,13 +32,117 @@ public class PingMonitor {
 		catch (Exception e) {
 			e.printStackTrace();
 		}
+		return -1;
 	}
 
 	public static void main(String[] args) {
-		
-		String ip = "google.com";
-		runSystemCommand("ping " + ip+" -t");
+		FrameGenerator window = new FrameGenerator("Ping Monitor"); 
+		window.setVisible(true);
+		window.setSize(400,400);
+	}
+}
 
-	
+class FrameGenerator extends Frame{
+	private int count = RanGen.generate();
+	private int averagePing;
+	private int pingData[];
+	private int high=200, mid=100, low=0;
+	public FrameGenerator(String title){
+		super(title);
+
+		pingData = new int[20];
+
+		for(int i = 1; i < 20; i++){
+			pingData[i] = -2;
+		}
+		pingData[0] = 0;
+
+		MyTimerTask myTask = new MyTimerTask();
+		Timer myTimer = new Timer();
+		myTimer.schedule(myTask, 1000, 1000);
+
+		addWindowListener(new WindowAdapter(){
+							public void windowClosing(WindowEvent we){
+								myTimer.cancel();
+								System.exit(0);
+							}
+						});
+	}
+
+	public void paint(Graphics g){
+		String ip = "google.com";
+		int ping = PingMonitor.runSystemCommand("ping " + ip+" -t");
+
+		g.fillRect(40,55,342,200);
+		int oldLineX = 40, oldLineY;
+		if(pingData[0]==-1){
+			oldLineY = 60;
+		}
+		else{
+			oldLineY = 260-pingData[0];
+		}
+
+		for(int i = 0; i < 20; i++){
+			if(pingData[i]==-2){
+				continue;
+			}
+			if(pingData[i]==-1){
+				g.setColor(Color.red);
+				g.drawLine(oldLineX, oldLineY, oldLineX=40+i*18, oldLineY=60);
+				continue;
+			}
+			g.setColor(Color.green);
+			g.drawLine(oldLineX, oldLineY, oldLineX=40+i*18, oldLineY=260-pingData[i]);
+		}
+		g.setColor(Color.black);
+		g.setFont(new Font("Arial",Font.PLAIN,10));
+		g.drawString(Integer.toString(high),20,70);
+		g.drawString(Integer.toString(mid),20,160);
+		g.drawString(Integer.toString(low),20,250);
+
+
+		//Ping : 20 | Average Ping : 23
+		g.setFont(new Font("Arial",Font.PLAIN,15));
+		g.drawString("Ping : "+count+"  |  "+"Average Ping : "+getAveragePing(),20,370);	
+	}
+
+	public int getAveragePing(){
+		int totalPing = 0;
+		int noOfPing = 20;
+		for(int i = 0; i < 20; i++){
+			if(pingData[i] == -1 || pingData[i] == -2){
+				noOfPing--;
+				continue;
+			}
+			totalPing += pingData[i];
+		}
+		if(noOfPing == 0){
+			return -1;
+		}
+		return totalPing/noOfPing;
+	}
+
+	class MyTimerTask extends TimerTask {
+		public void run() {
+			for(int i = 19; i > 0; i--){
+				pingData[i] = pingData[i-1];
+			}
+			pingData[0] = count;
+			
+			repaint();
+			count = RanGen.generate();
+		}	
+	}
+
+}
+
+
+class RanGen{
+	public static int generate(){
+		Random rn = new Random();
+		if(rn.nextInt(5)<2){
+			return -1;
+		}
+		return rn.nextInt(200)-1;
 	}
 }
